7a4d8be9ef231794d17109da8ef63c9d4d4dba92
Tanner Stokes <tanner@tannr.com>
2020-02-19 21:11:00 -0500

    Formatting

--- pageturn/src/main.ino
+++ pageturn/src/main.ino
@@ -10,13 +10,14 @@
   #include <SoftwareSerial.h>
 #endif
 
-#define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+#define MINIMUM_FIRMWARE_VERSION "0.6.6"
 #define BUTTON_PIN 12
 
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 
 // A small helper
-void error(const __FlashStringHelper*err) {
+void error(const __FlashStringHelper *err)
+{
   while (1);
 }
 
@@ -29,32 +30,37 @@ void setup(void)
 
   Serial.begin(115200);
 
-  if ( !ble.begin(true) )
+  if (!ble.begin(true))
   {
     error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
   }
 
   ble.echo(true);
 
-  if (! ble.sendCommandCheckOK(F( "AT+GAPDEVNAME=Bluefruit Keyboard" )) ) {
+  if (!ble.sendCommandCheckOK(F("AT+GAPDEVNAME=Bluefruit Keyboard")))
+  {
     error(F("Could not set device name?"));
   }
 
-  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
+  if (ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION))
   {
-    if ( !ble.sendCommandCheckOK(F( "AT+BleHIDEn=On" ))) {
+    if (!ble.sendCommandCheckOK(F("AT+BleHIDEn=On")))
+    {
       error(F("Could not enable Keyboard"));
     }
-  } else
+  }
+  else
   {
-    if (! ble.sendCommandCheckOK(F( "AT+BleKeyboardEn=On"  ))) {
+    if (!ble.sendCommandCheckOK(F("AT+BleKeyboardEn=On")))
+    {
       error(F("Could not enable Keyboard"));
     }
   }
 
   /* Add or remove service requires a reset */
   Serial.println(F("Performing a SW reset (service changes require a reset): "));
-  if (! ble.reset() ) {
+  if (!ble.reset())
+  {
     error(F("Couldn't reset??"));
   }
 
@@ -63,22 +69,24 @@ void setup(void)
 
 void loop(void)
 {
-  if(digitalRead(BUTTON_PIN)) {
-      Serial.println("BUTTON PRESSED");
+  if (digitalRead(BUTTON_PIN))
+  {
+    Serial.println("BUTTON PRESSED");
 
     // send the keyboard code command for right arrow
     ble.println("AT+BleKeyboardCode=00-00-4F");
     ble.println("AT+BleKeyboardCode=00-00");
 
-    if( ble.waitForOK() )
+    if (ble.waitForOK())
     {
-        Serial.println( F("OK!") );
-    }else
+      Serial.println(F("OK!"));
+    }
+    else
     {
-        Serial.println( F("FAILED!") );
+      Serial.println(F("FAILED!"));
     }
 
-      delay(500);
+    delay(500);
   }
 
   delay(10);
